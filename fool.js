b4efaac243b2807c28e7372d40f86eef062db54b
SeanJA <sean.sandy@gmail.com>
2011-04-05 15:31:34 -0700

    42 is a much better number, also, got rid of some of the repetition that was in there

--- fool.js
+++ fool.js
@@ -1,8 +1,9 @@
 //  Uncomment the next line if you want the prank to happen only when the url hash is #april
 //  if (document.location.hash.indexOf('april') > -1)
-    document.body.style.transform =
-    document.body.style.webkitTransform =
-    document.body.style.MozTransform =
-    document.body.style.OTransform =
-    document.body.style.msTransform =
-        'rotate(' + 45 * Math.ceil(Math.random() * 7) + 'deg)';
+(function(a){
+a.transform=
+a.webkitTransform=
+a.MozTransform=
+a.OTransform=
+a.msTransform='rotate('+42+'deg)';
+})(document.body.style);
